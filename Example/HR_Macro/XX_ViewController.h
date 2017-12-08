60e80c9381a56bb549f63b08debc5911b4c7acc1
obgniyum <muyingbo@risenb.com>
2017-12-08 14:15:16 +0800

    Initial commit

---
+++ Example/HR_Macro/XX_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  XX_ViewController.h
+//  HR_Macro
+//
+//  Created by obgniyum on 12/08/2017.
+//  Copyright (c) 2017 obgniyum. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XX_ViewController : UIViewController
+
+@end
